77e54862f1ab4b22f88875f30c99ecd7768609f3
i-sainty <isaacsainty@gmail.com>
2026-09-10 11:43:07 +0100

    Update txt to excel.py

--- src/txt to excel.py
+++ src/txt to excel.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
 print("Loading text file...")
-# Read the comma-separated text file
 df = pd.read_csv('cup98LRN.txt', sep=',', low_memory=False)
 
 print("Exporting to Excel...")
-# Save it as an Excel file
 df.to_excel('cup98LRN.xlsx', index=False)
 
 print("Conversion complete")
